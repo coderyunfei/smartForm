6892bc21217cde5556b94afef3439447f90b510f
wyf <yunfei.wang@xbongbong.com>
2023-03-21 10:29:08 +0800

    feat: 初始化项目

--- build/vite/plugins/component.js
+++ build/vite/plugins/component.js
@@ -2,7 +2,7 @@
  * @name  AutoRegistryComponents
  * @description 按需加载，自动引入组件
  */
-import Components from 'unplugin-vue-components/vite';
+import Components from "unplugin-vue-components/vite";
 // import {
 //   ElementPlusResolver,
 //   VueUseComponentsResolver,
@@ -12,10 +12,9 @@ import Components from 'unplugin-vue-components/vite';
 // } from 'unplugin-vue-components/resolvers';
 export const AutoRegistryComponents = () => {
   return Components({
-    dirs: ['src/components'],
-    extensions: ['vue'],
+    dirs: ["src/components"],
+    extensions: ["vue"],
     deep: true,
-    dts: 'types/components.d.ts',
     directoryAsNamespace: false,
     globalNamespaces: [],
     directives: true,
